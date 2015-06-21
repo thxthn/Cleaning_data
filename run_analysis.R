f7d50b9fe75f6162ed0c7ca9cb2409b3d45778a0
thxthn <thxthn@gmail.com>
2015-06-21 16:28:53 -0400

    R file

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,17 @@
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileURL, destfile = "dataset.zip", method = "libcurl")
-subject_train <- read.table(unz("dataset.zip","UCI HAR Dataset/train/subject_train.txt"))
-x_train <- read.table(unz("dataset.zip", "UCI HAR Dataset/train/X_train.txt"))
-y_train <- subject_train <- read.table(unz("dataset.zip","UCI HAR Dataset/train/y_train.txt"))
-subject_test <- read.table(unz("dataset.zip","UCI HAR Dataset/test/subject_test.txt"))
-x_test <- read.table(unz("dataset.zip", "UCI HAR Dataset/test/X_test.txt"))
-y_test <- read.table(unz("dataset.zip","UCI HAR Dataset/test/y_test.txt"))
-features <- read.table(unz("dataset.zip","UCI HAR Dataset/features.txt"))
-activity_labels <- read.table(unz("dataset.zip","UCI HAR Dataset/activity_labels.txt"))
+unzip("dataset.zip")
+Subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- subject_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+Subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 
 ## Merging the data sets 
-subject <- rbind(subject_train,subject_test)
+subject <- rbind(Subject_train,Subject_test)
 activity <- rbind(y_train,y_test)
 data <- rbind(x_train,x_test)
 mergedData <- cbind(data,subject,activity)
@@ -39,3 +40,6 @@ names(Data)<-gsub("BodyBody", "Body", names(Data))
 ## for each activity and each subject
 tidyData <- aggregate(.~ subject + activity,Data,mean,na.rm=T)
 tidyData <- tidyData[order(tidyData$subject,tidyData$activity),]
+
+## Write out data to table
+write.table(tidyData, file = "tidydata.txt",row.name=FALSE)
